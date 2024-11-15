21bbd8cebee3c3fa5b95625cf566ce501b0d0150
Mohieddine Essbai <mes-sbai01@educarex.es>
2024-11-15 12:46:32 +0100

    ejercicio Cambiar eurtos

---
+++ Paso-Monedas/src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent {
+euros:number=0;
+dolares:number=0;
+valorDol:number=0.95;
+Yenes:number=0;
+
+
+}
